81097a8e7a94aa1efb236dd46f3c3175c091c6d9
khsaad04 <khsaad0404@gmail.com>
2024-10-25 20:25:23 +0600

    feat: Implement generate_base16 function

--- src/main.rs
+++ src/main.rs
@@ -51,6 +51,8 @@ fn main() -> io::Result<()> {
         eprintln!("ERROR: Wallpaper `{}` not found", wp_path.to_str().unwrap());
     }
 
+    generate_base16(&mut manifest);
+
     for (_, file) in manifest.files.into_iter() {
         if let Some(template) = file.template {
             parse_file(&manifest.config, template, &file.target)?;
@@ -59,6 +61,51 @@ fn main() -> io::Result<()> {
     }
 
     Ok(())
+}
+
+fn blend_color(first: &str, second: &str, weight: f32) -> String {
+    let w = weight * 2.0 - 1.0;
+    let w1 = (w / 2.0) + 0.5;
+    let w2 = 1.0 - w1;
+    let c1_r = i64::from_str_radix(&first[..2], 16).unwrap();
+    let c1_g = i64::from_str_radix(&first[2..4], 16).unwrap();
+    let c1_b = i64::from_str_radix(&first[4..6], 16).unwrap();
+    let c2_r = i64::from_str_radix(&second[..2], 16).unwrap();
+    let c2_g = i64::from_str_radix(&second[2..4], 16).unwrap();
+    let c2_b = i64::from_str_radix(&second[4..6], 16).unwrap();
+    let r = (c1_r as f32 * w1 + c2_r as f32 * w2) as i64;
+    let g = (c1_g as f32 * w1 + c2_g as f32 * w2) as i64;
+    let b = (c1_b as f32 * w1 + c2_b as f32 * w2) as i64;
+    format!("{:x}{:x}{:x}", r, g, b).to_string()
+}
+
+fn generate_base16(config: &mut Manifest) {
+    let base16: HashMap<String, String> = HashMap::from([
+        ("base0".to_string(), "000000".to_string()),
+        ("base1".to_string(), "ff0000".to_string()),
+        ("base2".to_string(), "00ff00".to_string()),
+        ("base3".to_string(), "ffff00".to_string()),
+        ("base4".to_string(), "0000ff".to_string()),
+        ("base5".to_string(), "ff00ff".to_string()),
+        ("base6".to_string(), "00ffff".to_string()),
+        ("base7".to_string(), "ffffff".to_string()),
+        ("base8".to_string(), "000000".to_string()),
+        ("base9".to_string(), "ff0000".to_string()),
+        ("base10".to_string(), "00ff00".to_string()),
+        ("base11".to_string(), "ffff00".to_string()),
+        ("base12".to_string(), "0000ff".to_string()),
+        ("base13".to_string(), "ff00ff".to_string()),
+        ("base14".to_string(), "00ffff".to_string()),
+        ("base15".to_string(), "ffffff".to_string()),
+    ]);
+    for (k, v) in base16.into_iter() {
+        let mut weight: f32 = 0.3;
+        if k[4..].parse::<usize>().unwrap() > 7 {
+            weight = 0.5;
+        }
+        let new_color = blend_color(&v, config.config.get("primary").unwrap(), weight);
+        config.config.insert(k, new_color);
+    }
 }
 
 fn parse_file(
