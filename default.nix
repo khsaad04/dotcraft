d503de1d484ef2edabe873ee90a4bfb0aa2fcb7a
khsaad04 <khsaad0404@gmail.com>
2024-11-09 12:55:55 +0600

    feat: Add default.nix

---
+++ default.nix
@@ -0,0 +1,12 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
+pkgs.rustPlatform.buildRustPackage {
+  pname = manifest.name;
+  inherit (manifest) version;
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+}
