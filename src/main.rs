a1a977a42cc9336dcda6d23ef0272d067c6a74a9
khsaad04 <khsaad0404@gmail.com>
2025-01-06 12:58:00 +0600

    feat: Make destination path a required field

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,19 @@ use std::{
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Manifest {
-    wallpaper: Option<PathBuf>,
+    wallpaper: Option<String>,
     dark: Option<bool>,
     files: HashMap<String, File>,
 }
 
-type VarMap = HashMap<String, String>;
-
 #[derive(Debug, Deserialize, Serialize)]
 struct File {
     target: String,
-    dest: Option<PathBuf>,
-    template: Option<PathBuf>,
+    dest: String,
+    template: Option<String>,
 }
+
+type VarMap = HashMap<String, String>;
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
@@ -46,14 +46,14 @@ fn main() -> eyre::Result<()> {
     // Generate color scheme from wallpaper
     let mut config: VarMap = HashMap::new();
     if let Some(wallpaper) = manifest.wallpaper {
-        let wp_path = wallpaper
+        let wp_path = PathBuf::from(&wallpaper)
             .canonicalize()
-            .context(format!("Wallpaper `{}` not found", wallpaper.display()))?;
+            .context(format!("Wallpaper `{}` not found", &wallpaper))?;
         config.insert("wallpaper".to_string(), wp_path.display().to_string());
         let scheme = manifest.dark.unwrap_or(true);
         colors::generate_material_colors(wp_path, scheme, &mut config)?;
     } else if has_templates(&manifest) {
-        panic!("`wallpaper` is not set. Needed for color scheme generation");
+        eprintln!("ERROR: `wallpaper` is not set. Needed for color scheme generation");
     } else {
         println!("WARNING: Skipping color scheme generation.");
     }
@@ -68,7 +68,8 @@ fn main() -> eyre::Result<()> {
                 if let Some(file) = manifest.files.get(specified_name) {
                     run_sync_command(file, &config, force)?;
                 } else {
-                    panic!("`{}` not found", specified_name);
+                    eprintln!("ERROR: `{}` not found", specified_name);
+                    return Ok(());
                 }
             } else {
                 for (_, file) in manifest.files.iter() {
@@ -84,7 +85,8 @@ fn main() -> eyre::Result<()> {
                 if let Some(file) = manifest.files.get(specified_name) {
                     run_link_command(file, force)?;
                 } else {
-                    panic!("`{}` not found", specified_name);
+                    eprintln!("ERROR: `{}` not found", specified_name);
+                    return Ok(());
                 }
             } else {
                 for (_, file) in manifest.files.iter() {
@@ -100,7 +102,8 @@ fn main() -> eyre::Result<()> {
                 if let Some(file) = manifest.files.get(specified_name) {
                     run_generate_command(file, &config, force)?;
                 } else {
-                    panic!("`{}` not found", specified_name);
+                    eprintln!("ERROR: `{}` not found", specified_name);
+                    return Ok(());
                 }
             } else {
                 for (_, file) in manifest.files.iter() {
@@ -115,26 +118,18 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
+fn resolve_home_dir(path: &str) -> eyre::Result<PathBuf> {
+    let home_dir = std::env::var("HOME")?;
+    Ok(PathBuf::from(
+        path.replace('~', &home_dir).replace("$HOME", &home_dir),
+    ))
+}
+
 fn parse_paths(entry: &Path, file: &File) -> eyre::Result<(PathBuf, PathBuf)> {
     let target_path = PathBuf::from(&entry)
         .canonicalize()
         .context(format!("Target `{}` not found", &entry.display()))?;
-    let home_dir = PathBuf::from(std::env::var("HOME")?);
-    let dest_path = if let Some(dest) = &file.dest {
-        let dest = PathBuf::from(
-            dest.display()
-                .to_string()
-                .replace('~', home_dir.to_str().unwrap()),
-        )
-        .canonicalize()?;
-        if dest.is_dir() {
-            dest.join(entry.iter().last().unwrap())
-        } else {
-            dest
-        }
-    } else {
-        home_dir.join(entry)
-    };
+    let dest_path = resolve_home_dir(&file.dest)?.join(entry.iter().last().unwrap());
     if !dest_path.parent().unwrap().exists() {
         fs::create_dir_all(dest_path.parent().unwrap())?;
     }
@@ -142,7 +137,7 @@ fn parse_paths(entry: &Path, file: &File) -> eyre::Result<(PathBuf, PathBuf)> {
 }
 
 fn run_sync_command(file: &File, config: &VarMap, force: &bool) -> eyre::Result<()> {
-    let globbed_path = glob(&file.target).context(format!(
+    let globbed_path = glob(resolve_home_dir(&file.target)?.to_str().unwrap()).context(format!(
         "Failed to parse target `{}`. Invalid glob pattern",
         &file.target
     ))?;
@@ -150,10 +145,11 @@ fn run_sync_command(file: &File, config: &VarMap, force: &bool) -> eyre::Result<
         let entry = entry?;
         let (target_path, dest_path) = parse_paths(&entry, file)?;
         if let Some(template_path) = &file.template {
+            let template_path = PathBuf::from(template_path);
             if template_path.exists() {
-                generate_template(config, template_path, &target_path, force)?;
+                generate_template(config, &template_path, &target_path, force)?;
             } else {
-                println!("ERROR: Template `{}` not found", &template_path.display())
+                eprintln!("ERROR: Template `{}` not found", &template_path.display())
             }
         }
         symlink_dir_all(&target_path, &dest_path, force)?;
@@ -162,7 +158,7 @@ fn run_sync_command(file: &File, config: &VarMap, force: &bool) -> eyre::Result<
 }
 
 fn run_link_command(file: &File, force: &bool) -> eyre::Result<()> {
-    let globbed_path = glob(&file.target).context(format!(
+    let globbed_path = glob(resolve_home_dir(&file.target)?.to_str().unwrap()).context(format!(
         "Failed to parse target `{}`. Invalid glob pattern",
         &file.target
     ))?;
@@ -175,7 +171,7 @@ fn run_link_command(file: &File, force: &bool) -> eyre::Result<()> {
 }
 
 fn run_generate_command(file: &File, config: &VarMap, force: &bool) -> eyre::Result<()> {
-    let globbed_path = glob(&file.target).context(format!(
+    let globbed_path = glob(resolve_home_dir(&file.target)?.to_str().unwrap()).context(format!(
         "Failed to parse target `{}`. Invalid glob pattern",
         &file.target
     ))?;
@@ -183,10 +179,11 @@ fn run_generate_command(file: &File, config: &VarMap, force: &bool) -> eyre::Res
         let entry = entry?;
         let (target_path, _) = parse_paths(&entry, file)?;
         if let Some(template_path) = &file.template {
+            let template_path = PathBuf::from(template_path);
             if template_path.exists() {
-                generate_template(config, template_path, &target_path, force)?;
+                generate_template(config, &template_path, &target_path, force)?;
             } else {
-                println!("ERROR: Template `{}` not found", &template_path.display())
+                eprintln!("ERROR: Template `{}` not found", &template_path.display())
             }
         }
     }
@@ -299,7 +296,7 @@ fn symlink_file(target: &Path, dest: &Path, force_flag: &bool) -> eyre::Result<(
                     }
                 }
                 _ => {
-                    println!(
+                    eprintln!(
                         "ERROR: Failed to symlink `{}` -> `{}`. {}",
                         target.display(),
                         dest.display(),
