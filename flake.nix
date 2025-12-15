f3cc4f4055e90a5c482a56fb56b75727e77b6b65
khsaad04 <khsaad0404@gmail.com>
2025-12-15 15:59:01 +0600

    Remove nix shell

--- flake.nix
+++ flake.nix
@@ -5,14 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+    in {
       packages.${system}.default = import ./. { inherit pkgs; };
     };
 }
